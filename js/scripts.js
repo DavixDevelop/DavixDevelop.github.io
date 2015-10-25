16fd2fa0d2647b2c854ba1da6f2b83288298d753
DavixDevelop <davidrupnik23@gmail.com>
2015-10-25 21:07:18 +0100

    Updated Website

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 document.getElementById("foother_text").innerHTML = "@ 2015 DavixDevelop | Site in development";
+
+function httpGetAsync(theUrl, callback)  //Get file data
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function httpGet(theUrl)  //Get file data
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
