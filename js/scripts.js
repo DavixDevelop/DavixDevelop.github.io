cd18e79a8ad8ff085afff07b0dcae38c1519c6cb
DavixDevelop <davidrupnik23@gmail.com>
2015-10-29 17:54:02 +0100

    Updated Footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-document.getElementById("foother_text").innerHTML = "@ 2015 DavixDevelop";
+document.getElementById("foother_text").innerHTML = '<div class="container" style="padding-top:15.5px;padding-bottom:15.5px;"><div class="row text-center"><div class="col-xs-3"><a href="https://www.facebook.com/david.rupnik.397" target="_blank"><i class="fa fa-facebook fa-2x"></i></a></div><div class="col-xs-3"><a href="https://twitter.com/davidrupnik23" target="_blank"><i class="fa fa-twitter fa-2x"></i></a></div><div class="col-xs-3"><a href="http://www.twitch.tv/mepphotv" target="_blank"><i class="fa fa-twitch fa-2x"></i></a></div><div class="col-xs-3"><a href="https://instagram.com/davidrupnik/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a</div></div></div>';
 
 function httpGetAsync(theUrl, callback)  //Get file data
 {
