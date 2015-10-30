fa0c7e0b0bbc1291352a5be983bbb8325df9b16f
DavixDevelop <davidrupnik23@gmail.com>
2015-10-30 14:20:49 +0100

    Dynamic Update
    New dynamic loading system

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-document.getElementById("foother_text").innerHTML = '<div class="container" style="padding-top:15.5px;padding-bottom:15.5px;"><div class="row text-center"><div class="col-xs-3"><a href="https://www.facebook.com/david.rupnik.397" target="_blank"><i class="fa fa-facebook fa-2x"></i></a></div><div class="col-xs-3"><a href="https://twitter.com/davidrupnik23" target="_blank"><i class="fa fa-twitter fa-2x"></i></a></div><div class="col-xs-3"><a href="http://www.twitch.tv/mepphotv" target="_blank"><i class="fa fa-twitch fa-2x"></i></a></div><div class="col-xs-3"><a href="https://instagram.com/davidrupnik/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a</div></div></div>';
-
 function httpGetAsync(theUrl, callback)  //Get file data
 {
     var xmlHttp = new XMLHttpRequest();
